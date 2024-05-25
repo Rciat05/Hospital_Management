1125c0ed93c2f01d6c6edf639bcd96b8b7e33aaa
AlvinRHD <ezequielhernandes907@gmail.com>
2024-05-24 22:09:28 -0600

    New Project

---
+++ Hospital_Management2Demo/Hospital_Management2/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital_Management2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
