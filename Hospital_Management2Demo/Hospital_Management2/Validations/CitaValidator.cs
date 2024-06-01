26dec49c9ff2d9049a8b60c3026d342ec59c2820
AlvinRHD <ezequielhernandes907@gmail.com>
2024-06-01 15:57:28 -0600

    Modified create, delete, edit, index

---
+++ Hospital_Management2Demo/Hospital_Management2/Validations/CitaValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Hospital_Management2.Models;
+
+namespace Hospital_Management2.Validations
+{
+	public class CitaValidator : AbstractValidator<CitaModel>
+	{
+		public CitaValidator() {
+
+			RuleFor(cita => cita.PacienteID)
+				.NotEmpty().WithMessage("El ID del paciente es obligatorio")
+				.GreaterThan(0).WithMessage("El ID del paciente debe ser mayor que cero");
+
+			RuleFor(cita => cita.DoctorID)
+				.NotEmpty().WithMessage("El ID del doctor es obligatorio")
+				.GreaterThan(0).WithMessage("El ID del doctor debe ser mayor que cero");
+
+			RuleFor(cita => cita.FechaCita)
+				.NotEmpty().WithMessage("La fecha de contratación del doctor es obligatoria");
+
+			RuleFor(cita => cita.MotivoCita)
+				.NotEmpty().WithMessage("El motivo de la cita es obligatorio")
+				.Length(10, 200).WithMessage("El motivo de la cita debe tener entre 1 y 200 caracteres");
+
+			RuleFor(cita => cita.EstadoCitas)
+				.NotEmpty().WithMessage("El estado de la cita es obligatorio")
+				.Length(1).WithMessage("El estado de la cita debe ser de un solo carácter");
+		}
+	}
+}
