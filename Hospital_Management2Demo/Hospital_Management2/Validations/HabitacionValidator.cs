88d7498f08e7befc792a7cf4a6a1ddc6ca977f3d
AlvinRHD <ezequielhernandes907@gmail.com>
2024-06-01 10:31:08 -0600

    Agg create, edit, delete tabla habitación

--- Hospital_Management2Demo/Hospital_Management2/Validations/HabitacionValidator.cs
+++ Hospital_Management2Demo/Hospital_Management2/Validations/HabitacionValidator.cs
@@ -8,14 +8,14 @@ namespace Hospital_Management2.Validations
         public HabitacionValidator() 
         {
             RuleFor(habitacion => habitacion.NumeroHabitacion)
-                .Length(1, 3)
+                .Length(1, 10)
                 .NotNull().WithMessage("El numero de Habitacion es obligatorio");
 
-            RuleFor(habitacion => habitacion.NumeroHabitacion)
+            RuleFor(habitacion => habitacion.TipoHabitacion)
                 .Length(8, 15)
                 .NotNull().WithMessage("El Tipo de habitacion es obligatorio");
 
-            RuleFor(habitacion => habitacion.NumeroHabitacion)
+            RuleFor(habitacion => habitacion.EstadoHabitaciones)
                 .Length(1, 1)
                 .NotNull().WithMessage("El Estado de la habitacion es obligatorio");
         }
